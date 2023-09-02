5d563225210f36a134961406354b31010925df7d
Jonay Pelluz <jonay.pelluz@mad.io>
2023-09-02 10:18:26 +0200

    bugfix: trying to fix base path

--- next.config.js
+++ next.config.js
@@ -1,22 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      loader: 'default',
-      unoptimized: true,
-    },
-    basePath: '',
-    trailingSlash: true,
-    async redirects() {
-      return [
-        {
-          source: '/vacarisas/',
-          destination: '/vacarisses/',
-          permanent: true,
-        },
-      ];
-    }
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true,
+  images: {
+    loader: 'default',
+    unoptimized: true,
+  },
+  basePath: 'paseoperrosen/',
+  trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/vacarisas/',
+        destination: '/vacarisses/',
+        permanent: true,
+      },
+    ];
+  }
+};
+
+module.exports = nextConfig;
