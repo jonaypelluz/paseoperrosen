fea047dccc68445731044ddffca2bcbbc827d8f8
Jonay Pelluz <jonay.pelluz@mad.io>
2024-09-24 13:25:53 +0200

    feature: new post

--- src/config/posts.ts
+++ src/config/posts.ts
@@ -6,12 +6,22 @@ const posts: Record<string, { content: string; title: string }> = {
             <p>El paseo tiene un valor mucho mayor: es la oportunidad diaria que le das para explorar el mundo exterior, estimular su olfato, ejercitar su mente al exponerse a nuevos estímulos, interactuar con otros perros, y mantenerse en forma a través del ejercicio físico.</p>
             <h2>Beneficios de pasear a tu perro:</h2>
             <ol>
-                <li>Bienestar físico</li>
+                <li><a href="https://paseoperrosen.com/blog/el-bienestar-fisico-del-perro/">Bienestar físico</a></li>
                 <li>Bienestar mental</li>
             </ol>
             <p>Sin embargo, no todos los perros disfrutan de los paseos de la misma manera. A veces, problemas de comportamiento relacionados con el miedo o la agresividad hacia otros perros o personas pueden afectar la calidad de los paseos. En esos casos, es recomendable buscar la ayuda de un especialista en comportamiento canino para solucionar el problema.</p>`,
         title: '¿Por qué es importante pasear a tu perro diariamente?',
     },
+    'el-bienestar-fisico-del-perro': {
+        content: `<p>El bienestar físico del perro al caminar está relacionado con varios beneficios clave:</p>
+            <p><b>Control del peso:</b> Caminar ayuda a los perros a mantener un peso saludable. Se estima que con paseos regulares, un perro con sobrepeso puede reducir su peso en un 15%, lo que previene enfermedades relacionadas con la obesidad como problemas articulares y diabetes.</p>
+            <p><b>Mejora de la salud cardiovascular:</b> El ejercicio regular, como caminar, mejora la circulación sanguínea y reduce el riesgo de problemas cardíacos en los perros.</p>
+            <p><b>Incremento de energía:</b> Los paseos regulares fortalecen los músculos, mejoran la resistencia y aumentan la energía general de los perros, haciéndolos más activos.</p>
+            <p><b>Prevención de enfermedades:</b> Caminar regularmente puede ayudar a prevenir ciertos problemas de salud en los perros, como la hipertensión, enfermedades metabólicas y trastornos digestivos.</p>
+            <p><b>Mejora de la salud mental:</b> El ejercicio físico también combate el aburrimiento y el estrés, lo que evita comportamientos destructivos en casa. Un perro más relajado y feliz tiene una mejor calidad de vida.</p>
+            <p>Mantener una rutina de paseos y ejercicio es esencial para el bienestar físico general del perro.</p>`,
+        title: 'El bienestar físico del perro'
+    }
 };
 
 export default posts;
